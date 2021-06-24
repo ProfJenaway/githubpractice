e2f31216f585fb39e68e4f6034ee3e1f2e4cb91a
ProfJenaway <jenawaye@macomb.edu>
2021-06-24 14:29:28 -0400

    testing conflicts

---
+++ FirstTurtlePractice.py
@@ -0,0 +1,12 @@
+import turtle
+ollie = turtle
+ollie.shape("turtle")
+ollie.color("blue")
+ollie.forward(150)
+ollie.right(90)
+ollie.forward(150)
+ollie.right(90)
+ollie.forward(150)
+ollie.right(90)
+ollie.forward(150)
+ollie.write("That was fun!")
